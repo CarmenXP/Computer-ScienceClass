f8fba98f5130a8f27d8a3960cb337c985c7749d4
CarmenXP <xocuaposadas.carmen@gmail.com>
2020-08-30 22:23:33 -0500

    clase-4-part-1

--- clase4.js
+++ clase4.js
@@ -1,3 +1,19 @@
 //crear mi propio tiruriruriru..........tiruriruriru.......... slice
 //recrear tu propio filter
 //extra find, reduce, sort
+
+//función slice
+
+let array= [1,2,3,4,5,6,7]
+function slice(array, start, end) {
+    if(!end) end = array.length
+    else if (end<0) end= array.length-1 + end
+    const result= []
+    for (let i = start; i<end; i++){
+        result.push(array[i])
+    }
+    return result
+}
+
+let r = slice(array, 2, -1 )
+console.log (r)
